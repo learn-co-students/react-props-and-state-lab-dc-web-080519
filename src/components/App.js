4c822a053fe7b06be632d85ed6f4bdb09fdf5fde
Peyton Doyle <p6doyle@gmail.com>
2019-10-10 07:42:23 -0400

    solved lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
@@ -15,6 +14,28 @@ class App extends React.Component {
     }
   }
 
+onChangeType = (e) => {
+  // console.log(e.target.value)
+  this.setState({filters: {type: e.target.value}})
+}
+
+onFindPetsClick = () => {
+  // let url = '/api/pets'
+  // let type = this.state.filters.type
+  // url += (type !== "all" ? `?type=${type}` : '')
+  let yolo = (this.state.filters.type === "all" ? "/api/pets" : `/api/pets?type=${this.state.filters.type}`)
+  fetch(yolo)
+  .then(response => response.json())
+  .then(petsData => {this.setState({pets: petsData})})
+}
+
+onAdoptPet = (id) => {
+  let newPets = this.state.pets.map(pet => {
+    return (pet.id === id ? {...pet, isAdopted: true} : pet)
+  })
+  this.setState({pets: newPets})
+}
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +45,14 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+              onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser
+              pets={this.state.pets}
+              onAdoptPet={this.onAdoptPet}
+              />
             </div>
           </div>
         </div>
